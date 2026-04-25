6c1ebf714b762d1721ea0a3ec4e6e064062edcd2
Yerko Maldonado <yerkomaldonado47@gmail.com>
2026-04-25 00:57:46 -0400

    cambio login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -28,14 +28,14 @@ export default function Login() {
       <div className="absolute inset-0 opacity-10 bg-[url('/leaves.png')] bg-cover bg-center" />
 
       {/* 📱 CONTENIDO */}
-      <div className="relative z-10 flex flex-col items-center text-center px-6 -mt-10">
+      <div className="relative z-10 flex flex-col items-center text-center px-6 -mt-40">
         {/* 🧠 LOGO GRANDE */}
         <Image
           src="/Estoy contigo.png"
           alt="logo"
-          width={400}
-          height={400}
-          className="mb-6"
+          width={220}
+          height={220}
+          className="mb-4 drop-shadow-xl rounded-full shadow-lg"
         />
 
         {/* BOTONES */}
